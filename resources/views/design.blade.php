3b48161079c06d298234692d62e2f8533a82d2c2
Erica Andrew <erica.nicole.andrew@gmail.com>
2019-12-27 18:01:39 -0700

    Added an image to the design page

--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -4,9 +4,11 @@
 	<div class="dev-wrapper py-5" style="margin-top: 75px;">
 		<div class="container boxes">
 			<div class="content-box">
-				<div class="box">
-
-				</div>
+				<a href="https://www.isolvedpartner.com">
+					<div class="box d-flex align-items-center">
+						<img src="/img/layout/sites/isolved_network_logo.png">
+					</div>
+				</a>
 			</div>
 		</div>
 	</div>
